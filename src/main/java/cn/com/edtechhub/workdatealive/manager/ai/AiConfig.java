5740286a063c391b8b1be24ca89d21469dcbecf7
limou3434 <898738804@qq.com>
2025-06-13 00:41:55 +0800

    初始化对话管理其

---
+++ src/main/java/cn/com/edtechhub/workdatealive/manager/ai/AiConfig.java
@@ -0,0 +1,50 @@
+package cn.com.edtechhub.workdatealive.manager.ai;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * AI 配置类
+ *
+ * @author <a href="https://github.com/limou3434">limou3434</a>
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.ai.dashscope")
+@Data
+@Slf4j
+public class AiConfig {
+
+    /**
+     * 密钥
+     */
+    private String apiKey;
+
+    /**
+     * 系统提示词
+     */
+    private String systemPrompt =
+            "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
+                    "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
+                    "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
+                    "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+
+    /**
+     * 打印配置
+     */
+    @PostConstruct
+    public void printConfig() {
+        log.debug("[AIConfig] 当前项目 AI 密钥已加载为 {}", this.apiKey);
+        log.debug("[AIConfig] 当前项目 AI 提示词语已设置为 {}", this.systemPrompt);
+    }
+
+}
---
+++ src/test/java/cn/com/edtechhub/workdatealive/manager/ai/AiManagerTest.java
@@ -0,0 +1,32 @@
+package cn.com.edtechhub.workdatealive.manager.ai;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+class AiManagerTest {
+
+    @Resource
+    private AiManager aiManager;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员鱼皮";
+        String answer = aiManager.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（编程导航）更爱我";
+        answer = aiManager.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = aiManager.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
